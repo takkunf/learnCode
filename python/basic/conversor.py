df2432f9cb794964622ea3ed53d311c754e9bdac
Takumi Fujii <takn.fujii@gmail.com>
2024-01-29 12:40:46 -0400

    arreglando fallas

--- python/basic/conversor.py
+++ python/basic/conversor.py
@@ -7,22 +7,22 @@ def main():
     Ingrese la opcion de la moneda que desea convertir: """))
 
     if moneda == 1 :
-        valor = float(input(print("Cuantos Bs desea cambiar a $? :  ")))
+        valor = float(input("Cuantos Bs desea cambiar a $?: "))
         dolar = valor / 6.9
         print("Su total seria de " + str(dolar) + "$")
 
     elif moneda == 2 :
-        valor = float(input(print("Cuantos ¥ desea cambiar a $? :  ")))
+        valor = float(input("Cuantos ¥ desea cambiar a $?: "))
         dolar = valor / 109.91
         print("Su total seria de " + str(dolar) + "$")
 
     elif moneda == 3 :
-        valor = float(input(print("Cuantos R$ desea cambiar a $? :  ")))
+        valor = float(input("Cuantos R$ desea cambiar a $?: "))
         dolar = valor / 5.32
         print("Su total seria de " + str(dolar) + "$")
 
     else:
-        print("Elige una opcion valida ctm!")
+        print("Elige una opcion valida subnormal!")
 
 
 
--- python/basic/game.py
+++ python/basic/game.py
@@ -2,28 +2,27 @@ import random
 
 
 def main() :
-    life = 4
-    rand = random.randint(1, 100)
-    chosenone = int(input("Choose a number from 1 to 100, you have 5 lives :  "))
+    life = 5
+    r = random.randint(1, 100)
+    num = int(input("Choose a number from 1 to 100, you have 5 lives :  "))
     
-    while rand != chosenone and life != 0 :
+    while r != num and life != 0 :
         
-
-        if rand > chosenone :
+        if r > num :
             print("Choose a higher number, you have "  + str(life) + " lives", end = " ")
 
         else:
-            print("Choose a lower number, you have"  + str(life) + " lives", end = " ")
+            print("Choose a lower number, you have "  + str(life) + " lives", end = " ")
         
+        num = int(input(" :  "))
         life -= 1
-        chosenone = int(input(" :  "))
         
 
-    if life != 0 :
+    if r == num :
         print("You win!!")
     
     else :
-        print("You lose... :c The number was " + str(rand))
+        print("You lose... :c The number was " + str(r))
 
 
 
